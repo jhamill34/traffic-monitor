8ff4ad15e63825fbf0206e458b38448be557364b
Joshua Rasmussen <joshuarasmussen@joshuas-imac.lan>
2019-07-31 17:36:51 -0700

    Add timezone to cron

--- backend/src/main/java/com/traffic/schedules/CommuteSchedule.java
+++ backend/src/main/java/com/traffic/schedules/CommuteSchedule.java
@@ -6,6 +6,8 @@ import com.traffic.models.TrafficSnapshot;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CommuteSchedule {
+    private Logger logger = LoggerFactory.getLogger(CommuteSchedule.class);
+
     @Value("${locations.home}")
     private String home;
 
@@ -26,15 +30,17 @@ public class CommuteSchedule {
     private GoogleMapsService googleMapsService;
 
     // Will Run every Monday through Friday afrom 5am to 7am every 5 minutes
-    @Scheduled(cron = "0 */5 5-7 * * 1-5")
+    @Scheduled(cron = "0 */5 5-7 * * MON-FRI", zone="GMT-8")
     public void morningCommuteTask() {
+        logger.info("Fetching morning commute time...");
         List<TrafficSnapshot> routes = googleMapsService.fetchSnapshot(home, work);
         trafficSnapshotService.storeAll(routes);
     }
 
     // Will Run every Monday through Friday afrom 3pm to 7pm every 5 minutes
-    @Scheduled(cron = "0 */5 15-19 * * 1-5")
+    @Scheduled(cron = "0 */5 15-19 * * MON-FRI", zone="GMT-8")
     public void afternoonCommuteTask() {
+        logger.info("Fetching afternoon commute time...");
         List<TrafficSnapshot> routes = googleMapsService.fetchSnapshot(work, home);
         trafficSnapshotService.storeAll(routes);
     }
